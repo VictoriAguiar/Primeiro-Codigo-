58fd9e8958c0c501c20191a1b6de1b2d662ae182
Victoria Aguiar <115103017+VictoriAguiar@users.noreply.github.com>
2022-10-05 14:34:10 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main()
+
+{
+    int a, b, soma;
+    printf("Hello World");
+    printf("\n""Digite seu nome: ");
+    scanf ("%d", &a );
+    
+}
